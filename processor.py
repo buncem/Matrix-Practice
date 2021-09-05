567e266d90c0c73e43df50e1b6f60fdc22bd81e3
Max Bunce <maximilienbunce@gmail.com>
2021-09-05 08:06:04 -0600

    implement class structure

--- processor.py
+++ processor.py
@@ -1,45 +1,63 @@
-def dimension_builder():
-    dimensions_str = input().split()
-    return [int(n) for n in dimensions_str]
+class Matrix:
+    def __init__(self, which_matrix=None):
+        self.dimensions = None
+        self.matrix = None
+        self.which_matrix = which_matrix
 
-def matrix_builder(dimensions):
-    matrix = []
-    for i in range(dimensions[0]):
-        row_str = input().split()
-        row = [int(n) for n in row_str]
-        matrix.append(row)
-    return matrix
+    def dimension_builder(self):
+        if self.which_matrix is None:
+            dimensions_str = input(f'Enter size of matrix: ').split()
+        else:
+            dimensions_str = input(f'Enter size of {self.which_matrix} matrix: ').split()
+        self.dimensions = [int(n) for n in dimensions_str]
 
-def matrix_sum(matrix_1, matrix_2):
-    answer = []
-    for n in range(len(matrix_1)):
-        row = [matrix_1[n][m] + matrix_2[n][m] for m in range(len(matrix_1[0]))]
-        answer.append(row)
-    return answer
+    def matrix_builder(self):
+        if self.which_matrix is None:
+            print('Enter matrix:')
+        else:
+            print(f'Enter {self.which_matrix} matrix:')
+        matrix = []
+        for i in range(self.dimensions[0]):
+            row_str = input().split()
+            row = [int(n) for n in row_str]
+            matrix.append(row)
+        self.matrix = matrix
 
-def matrix_multiplication(matrix, constant):
-    answer = []
-    for n in range(len(matrix)):
-        row = [constant * matrix[n][m] for m in range(len(matrix[0]))]
-        answer.append(row)
-    return answer
+    def print_matrix(self):
+        for n in range(len(self.matrix)):
+            print(' '.join([str(m) for m in self.matrix[n]]))
 
-def print_matrix(matrix):
-    for n in range(len(matrix)):
-        print(' '.join([str(m) for m in matrix[n]]))
+class MatrixAction(Matrix):
+    def matrix_sum(self, matrix_1, matrix_2):
+        answer = []
+        for n in range(len(matrix_1.matrix)):
+            row = [matrix_1.matrix[n][m] + matrix_2.matrix[n][m] for m in range(len(matrix_1.matrix[0]))]
+            answer.append(row)
+        self.matrix = answer
 
-dimension_1 = dimension_builder()
-matrix = matrix_builder(dimension_1)
-constant = int(input())
-matrix_multiplication = matrix_multiplication(matrix, constant)
-print_matrix(matrix_multiplication)
+    def matrix_constant_multiplication(self, matrix):
+        constant = int(input('Enter constant: '))
+        answer = []
+        for n in range(len(matrix.matrix)):
+            row = [constant * matrix.matrix[n][m] for m in range(len(matrix.matrix[0]))]
+            answer.append(row)
+        self.matrix = answer
 
-# dimensions_1 = dimension_builder()
-# matrix_1 = matrix_builder(dimensions_1)
-# dimensions_2 = dimension_builder()
-# matrix_2 = matrix_builder(dimensions_2)
-# if dimensions_1 == dimensions_2:
-#     matrix_sum = matrix_sum(matrix_1, matrix_2)
-#     print_matrix(matrix_sum)
-# else:
-#     print('ERROR')
+#class Menu:
+
+matrix_1 = Matrix(which_matrix='first')
+matrix_1.dimension_builder()
+matrix_1.matrix_builder()
+matrix_solution = MatrixAction()
+matrix_solution.matrix_constant_multiplication(matrix_1)
+matrix_solution.print_matrix()
+
+# matrix_1 = Matrix(which_matrix='first')
+# matrix_1.dimension_builder()
+# matrix_1.matrix_builder()
+# matrix_2 = Matrix(which_matrix='second')
+# matrix_2.dimension_builder()
+# matrix_2.matrix_builder()
+# matrix_solution = MatrixAction()
+# matrix_solution.matrix_sum(matrix_1, matrix_2)
+# matrix_solution.print_matrix()
